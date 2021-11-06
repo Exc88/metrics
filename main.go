84b9ee145b5a547c9fb94d3c4bccb09dcacc7e70
Exc88 <whitehuffman@gmail.com>
2021-11-06 06:58:24 +0300

    fixed docker cfg

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io/ioutil"
+	"net/http"
+)
+
+type Metrics struct {
+	RequestTotal *prometheus.CounterVec
+	RequestDuration *prometheus.HistogramVec
+}
+
+var metrics Metrics
+
 func main(){
 
+
+http.Handle("/metrics",promhttp.Handler())
+http.ListenAndServe(":2112",nil)
+}
+
+func createMetrics(){
+	metrics.RequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "metrics_app_requests_total",
+		Help: "Кол-во запросов к серверу",
+	},[]string{"method","handler","code"},)
+metrics.RequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{...},
+[]string{"method","handler","code"},)
+prometheus.MustRegister(metrics.RequestTotal)
+prometheus.MustRegister(metrics.RequestTotal)
 }
